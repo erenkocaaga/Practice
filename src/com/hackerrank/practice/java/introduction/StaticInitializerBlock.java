53dd35f4a7c8c2d154d4617268636163fab687f2
erenkocaaga <eren@trachiasports.com>
2020-02-10 13:10:04 +0300

    Java Practice

---
+++ src/com/hackerrank/practice/java/introduction/StaticInitializerBlock.java
@@ -0,0 +1,36 @@
+package com.hackerrank.practice.java.introduction;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class StaticInitializerBlock {
+    static Scanner in=new Scanner(System.in); 
+    static int B=in.nextInt(); 
+    static int H=in.nextInt(); 
+    static boolean flag=true;
+
+    static{
+        try{
+            if(B <= 0 || H <= 0){
+                flag = false;
+                throw new Exception("Breadth and height must be positive");
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
