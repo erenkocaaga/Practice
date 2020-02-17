9cf61865d4829c7044c4ef4ec08538dd0befff0e
erenkocaaga <eren@trachiasports.com>
2020-02-17 11:51:29 +0300

    JUnit updated with new examples

--- src/com/tutorialspoint/junit/setup/TestJunit.java
+++ src/com/tutorialspoint/junit/setup/TestJunit.java
@@ -1,12 +1,16 @@
 package com.tutorialspoint.junit.setup;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestJunit {
-   @Test
 	
-   public void testAdd() {
-      String str = "Junit is working fine";
-      assertEquals("Junit is working fine",str);
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {
+	  message = "New Word";
+      assertEquals(message,messageUtil.printMessage());
    }
 }
