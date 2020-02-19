9e68ddeec5b16d6b95d1901638160e5aa5884473
erenkocaaga <eren@trachiasports.com>
2020-02-19 08:32:58 +0300

    Bridge Pattern Demo added

---
+++ src/com/tutorialspoint/designpattern/bridgepattern/RedCircle.java
@@ -0,0 +1,11 @@
+package com.tutorialspoint.designpattern.bridgepattern;
+
+public class RedCircle implements DrawAPI {
+
+	@Override
+	public void drawCircle(int radius, int x, int y) {
+		// TODO Auto-generated method stub
+		System.out.println("Drawing Circle[ color: red, radius: " 
+				+ radius + ",x: " + x + ", " + y +  "]"); 
+	}
+}
