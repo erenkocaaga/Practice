b7b080a2faa8a6098e02b0fb48c4d9ce11ffba91
erenkocaaga <eren@trachiasports.com>
2020-02-21 16:44:59 +0300

    mystack changed

--- src/com/crackingthecodinginterview/stackandqueues/MyStack.java
+++ src/com/crackingthecodinginterview/stackandqueues/MyStack.java
@@ -1,5 +1,45 @@
 package com.crackingthecodinginterview.stackandqueues;
 
-public class MyStack {
+import java.util.EmptyStackException;
+
+public class MyStack <T> {
+	
+	private static class StackNode<T>{
+		private T data; 
+		private StackNode<T> next; 
+		private StackNode<T> top;
+
+		
+		public StackNode(T data)
+		{
+			this.data = data;
+		}
+		
+		public T pop() {
+			if(top == null) throw new EmptyStackException();
+			T item = top.data;
+			top = top.next; 
+			return item; 
+		}
+		
+		public void push(T item)
+		{
+			StackNode<T> t = new StackNode<T>(item); 
+			t.next = top; 
+			top = t;
+		}
+		
+		public T peek() 
+		{
+			if(top == null) throw new EmptyStackException();
+			return top.data;
+		}
+		
+		public boolean isEmpty()
+		{
+			return top == null; 
+		}
+		
+	}
 
 }
